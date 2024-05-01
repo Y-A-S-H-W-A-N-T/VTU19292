e9e92006903aa8671596512aac22553e78703eb4
raoyashwant <raoyashwant132@gmail.com>
2024-05-01 22:19:30 +0530

    User will only get 10 values initially, he can send empty data, he will still recieve output. Later he can choose n, min, max parameters

--- Question 1/frontend/src/components/products.js
+++ Question 1/frontend/src/components/products.js
@@ -36,58 +36,67 @@ function Products() {
 
 
   return (
-    <div className="container">
-        <input type='text' placeholder='Number of products' onChange={(e) => setN(e.target.value)} className="input-field" />
-        <input type='text' placeholder='Minimum price' onChange={(e) => setMin(e.target.value)} className="input-field" />
-        <input type='text' placeholder='Maximum price' onChange={(e) => setMax(e.target.value)} className="input-field" />
-        <select value={company} onChange={(e)=>setCompany(e.target.value)}>
-                <option value="AMZ">AMZ</option>
-                <option value="FLP">FLP</option>
-                <option value="SNP">SNP</option>
-                <option value="MYN">MYN</option>
-                <option value="AZO">AZO</option>
-        </select>
-        <select value={type} onChange={(e)=>setType(e.target.value)}>
-                <option value="Phone">PHONE</option>
-                <option value="Computer">COMPUTER</option>
-                <option value="Tv">TV</option>
-                <option value="Earphone">EARPHONE</option>
-                <option value="Tablet">TABLET</option>
-                <option value="Charger">CHARGER</option>
-                <option value="Mouse">MOUSE</option>
-                <option value="keypad">KEYPAD</option>
-                <option value="Bluetooth">BLUETOOTH</option>
-                <option value="Pendrive">PENDRIVE</option>
-                <option value="Remote">REMOTE</option>
-                <option value="Speaker">SPEAKER</option>
-                <option value="Headset">HEADSET</option>
-                <option value="Laptop">LAPTOP</option>
-                <option value="Pc">PC</option>
-        </select>
-        <button onClick={GetProducts} className="btn">Get Products</button>
-
-        <div className="product-list">
-            {products &&
-                products.map((val, ind) => (
-                    <div>
-                        <div className="product" key={ind} onClick={()=>{
-                            SHOW()
-                            setVal(val)
-                        }}>
-                            <div className='Each'>
-                            <div style={{display: 'flex', justifyContent: 'center'}}>
-                                <p>{val.productName}</p><br></br>
-                            </div>
-                            <p>Price: ${val.price}</p><br/>
-                            <p>Rating: {val.rating}</p><br/>
-                            </div>
-                        </div>
-                    </div>
-                ))}
+    <div className='ALL'>
                 <div>
-                    {item && <Item item={itemval} SHOW={SHOW} open={item}/>}
+                    <h1>SHOP ELECTRONICS</h1>
                 </div>
-        </div>
+            <div className="container">
+                <label>Products:</label><input type='text' placeholder='Number of products' onChange={(e) => setN(e.target.value)} className="input-field" />
+                <label>Starting Price:</label><input type='text' placeholder='Minimum price' onChange={(e) => setMin(e.target.value)} className="input-field" />
+                <label>Budget:</label><input type='text' placeholder='Maximum price' onChange={(e) => setMax(e.target.value)} className="input-field" /><br></br>
+                <br></br>
+                <label>Select Company</label>
+                <select value={company} onChange={(e)=>setCompany(e.target.value)}>
+                        <option value="AMZ">AMZ</option>
+                        <option value="FLP">FLP</option>
+                        <option value="SNP">SNP</option>
+                        <option value="MYN">MYN</option>
+                        <option value="AZO">AZO</option>
+                </select>
+                <br></br>
+                <label>Select Electronics</label>
+                <select value={type} onChange={(e)=>setType(e.target.value)}>
+                        <option value="Phone">PHONE</option>
+                        <option value="Computer">COMPUTER</option>
+                        <option value="Tv">TV</option>
+                        <option value="Earphone">EARPHONE</option>
+                        <option value="Tablet">TABLET</option>
+                        <option value="Charger">CHARGER</option>
+                        <option value="Mouse">MOUSE</option>
+                        <option value="keypad">KEYPAD</option>
+                        <option value="Bluetooth">BLUETOOTH</option>
+                        <option value="Pendrive">PENDRIVE</option>
+                        <option value="Remote">REMOTE</option>
+                        <option value="Speaker">SPEAKER</option>
+                        <option value="Headset">HEADSET</option>
+                        <option value="Laptop">LAPTOP</option>
+                        <option value="Pc">PC</option>
+                </select><br></br>
+                <button onClick={GetProducts} className="btn">Get Products</button>
+
+                <div className="product-list">
+                    {products &&
+                        products.map((val, ind) => (
+                            <div>
+                                <div className="product" key={ind} onClick={()=>{
+                                    SHOW()
+                                    setVal(val)
+                                }}>
+                                    <div className='Each'>
+                                    <div style={{display: 'flex', justifyContent: 'center'}}>
+                                        <p>{val.productName}</p><br></br>
+                                    </div>
+                                    <p>Price: ${val.price}</p><br/>
+                                    <p>Rating: {val.rating}</p><br/>
+                                    </div>
+                                </div>
+                            </div>
+                        ))}
+                        <div>
+                            {item && <Item item={itemval} SHOW={SHOW} open={item}/>}
+                        </div>
+                </div>
+            </div>
     </div>
   )
 }
